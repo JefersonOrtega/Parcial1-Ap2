17f07a55c17a0fa5ac6e08c8c1448c0b9118846b
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-06-10 20:23:07 -0300

    Creando Entidad

---
+++ Parcial1-AP2/Models/Articulos.cs
@@ -0,0 +1,23 @@
+﻿using Parcial1_AP2.Pages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "Debe ingresar la descripción del artículo")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Debe ingresar la existencia del articulo")]
+        public int Existencia { get; set; }
+        [Required(ErrorMessage = "Debe ingresar el Costo del artículo")]
+        public double Costo { get; set; }
+        public double ValorInventario { get; set; }
+
+    }
+}
