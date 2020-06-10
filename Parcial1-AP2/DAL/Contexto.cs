29824d6d09631632613c8699aa60314ccaf2b463
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-06-10 13:32:57 -0300

    Add project files.

---
+++ Parcial1-AP2/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_AP2.DAL
+{
+    public class Contexto : DbContext   
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Parcial1Ap2.db");
+                
+        }
+    }
+}
