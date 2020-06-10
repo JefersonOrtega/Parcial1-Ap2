21a3c78d2633bd2ab5e2770b18b52d29be6a3e68
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-06-10 20:25:10 -0300

    Agregando Entidad a Contexto

--- Parcial1-AP2/DAL/Contexto.cs
+++ Parcial1-AP2/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace Parcial1_AP2.DAL
 {
     public class Contexto : DbContext   
     {
-
+        public DbSet<Articulos> Articulos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
