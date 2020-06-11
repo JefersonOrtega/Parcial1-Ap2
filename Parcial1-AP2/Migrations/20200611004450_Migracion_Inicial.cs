963c7082afca673437d3863850408d45aa05ceb6
Jeferson Raul Ortega Brito <jeferson_ortega@ucne.edu.do>
2020-06-10 21:46:06 -0300

    Creando Migracion y Base de Datos

---
+++ Parcial1-AP2/Migrations/20200611004450_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_AP2.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<double>(nullable: false),
+                    ValorInventario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
